a178df860a008a0c339f67358fb30c5a966f518c
Lennyb940002 <146711861+Lennyb940002@users.noreply.github.com>
2025-01-28 09:45:23 +0100

    Update index.js

--- index.js
+++ index.js
@@ -22,9 +22,7 @@ mongoose.connect(process.env.MONGODB_URI, {
 });
 
 app.use(cors({
-  origin: [
-    "https://www.myminima.fr",
-  ],
+  origin: 'https://www.myminima.fr',
   credentials: true,
   exposedHeaders: ["set-cookie"],
 }));
