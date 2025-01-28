ccb692445ec0ef73b8acc4bb6c7e9be647564e8e
Lennyb940002 <146711861+Lennyb940002@users.noreply.github.com>
2025-01-28 13:57:08 +0100

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function App() {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    setIsSubmitting(true);
+    setIsSubmitting(true);//
     setErrorMessage("");
     setSubmitStatus(null);
 
